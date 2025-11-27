f25fd463b4971f71f874bc3c585033128745ea40
Tejaswani Kolasani <tejaswani0824@gmail.com>
2025-11-27 00:21:34 -0800

    design changes loading and color

--- frontend/src/components/Budget.jsx
+++ frontend/src/components/Budget.jsx
@@ -1393,7 +1393,20 @@ function Budget() {
   if (loading) {
     return (
       <div className="bg-[#EFEFEF] min-h-screen flex items-center justify-center">
-        <div className="text-[15px] md:text-lg text-gray-600 font-sans">Loading budget...</div>
+        {/* <div className="text-[15px] md:text-lg text-gray-600 font-sans">Loading budget...</div> */}
+        <svg width="126" height="126" viewBox="0 0 126 126" fill="none" xmlns="http://www.w3.org/2000/svg">
+<circle cx="62.5234" cy="62.5241" r="33.601" stroke="#238D88" stroke-opacity="0.6" stroke-width="3.40264"/>
+<circle cx="62.5235" cy="62.5243" r="47.2116" stroke="#238D88" stroke-opacity="0.4" stroke-width="3.40264"/>
+<circle cx="62.5234" cy="62.5235" r="60.8221" stroke="#238D88" stroke-opacity="0.1" stroke-width="3.40264"/>
+<rect x="39.981" y="40.8323" width="44.2343" height="44.2343" rx="22.1171" fill="#238D88"/>
+<path d="M55.809 62.4475C55.809 62.4475 62.1272 64.687 62.0319 70.605" stroke="white" stroke-width="2.38243" stroke-miterlimit="10" stroke-linecap="round"/>
+<path d="M54.8129 68.6697C50.92 68.6697 47.7561 65.5058 47.7561 61.6128V54.5608H54.8129C58.7059 54.5608 61.8698 57.7247 61.8698 61.6176C61.8698 65.5058 58.7059 68.6697 54.8129 68.6697ZM50.129 61.6128C50.129 64.1954 52.2303 66.2968 54.8129 66.2968C57.3955 66.2968 59.4968 64.1954 59.4968 61.6128C59.4968 59.0303 57.3955 56.9289 54.8129 56.9289H50.129V61.6128Z" fill="white"/>
+<path d="M54.8132 54.6804C58.6441 54.6804 61.7509 57.7871 61.7509 61.6181C61.7509 65.4491 58.6441 68.5558 54.8132 68.5558C50.9822 68.551 47.8755 65.4443 47.8755 61.6133V54.6804H54.8132ZM54.8132 66.4163C57.4672 66.4163 59.6162 64.2674 59.6162 61.6133C59.6162 58.9593 57.4672 56.8103 54.8132 56.8103H50.0101V61.6133C50.0101 64.2674 52.1591 66.4163 54.8132 66.4163ZM54.8132 54.4421H47.8755H47.6372V54.6804V61.6181C47.6372 65.573 50.8583 68.794 54.8132 68.794C58.768 68.794 61.9891 65.573 61.9891 61.6181C61.9891 57.6584 58.768 54.4421 54.8132 54.4421ZM50.2484 57.0533H54.8132C57.329 57.0533 59.3779 59.1022 59.3779 61.6181C59.3779 64.134 57.329 66.1829 54.8132 66.1829C52.2973 66.1829 50.2484 64.134 50.2484 61.6181V57.0533Z" fill="white"/>
+<path d="M69.3842 68.6698C65.4913 68.6698 62.3274 65.5059 62.3274 61.613C62.3274 57.72 65.4913 54.5562 69.3842 54.5562H76.441V61.613C76.441 65.5059 73.2771 68.6698 69.3842 68.6698ZM69.3842 56.9338C66.8016 56.9338 64.7003 59.0352 64.7003 61.6177C64.7003 64.2003 66.8016 66.3016 69.3842 66.3016C71.9668 66.3016 74.0681 64.2003 74.0681 61.6177V56.9338H69.3842Z" fill="white"/>
+<path d="M76.3215 54.6804V61.6181C76.3215 65.4491 73.2148 68.5558 69.3838 68.5558C65.5528 68.5558 62.4461 65.4491 62.4461 61.6181C62.4461 57.7871 65.5528 54.6804 69.3838 54.6804H76.3215ZM69.3838 66.4163C72.0379 66.4163 74.1869 64.2674 74.1869 61.6133V56.8151H69.3838C66.7298 56.8151 64.5808 58.964 64.5808 61.6181C64.5808 64.2674 66.7345 66.4163 69.3838 66.4163ZM76.5598 54.4421H76.3215H69.3838C65.429 54.4421 62.2079 57.6632 62.2079 61.6181C62.2079 65.573 65.429 68.794 69.3838 68.794C73.3387 68.794 76.5598 65.573 76.5598 61.6181V54.6804V54.4421ZM69.3838 66.1781C66.868 66.1781 64.8191 64.1292 64.8191 61.6133C64.8191 59.0974 66.868 57.0485 69.3838 57.0485H73.9486V61.6133C73.9486 64.1292 71.8997 66.1781 69.3838 66.1781Z" fill="white"/>
+<path d="M68.521 62.4475C68.521 62.4475 61.9359 64.687 62.0359 70.605" stroke="white" stroke-width="2.38243" stroke-miterlimit="10" stroke-linecap="round"/>
+</svg>
+
       </div>
     );
   }
@@ -1428,12 +1441,12 @@ function Budget() {
             <h2 className="text-[18px] md:text-[24px] font-semibold text-gray-800 m-0 font-sans">Budget Overview</h2>
             <button 
   onClick={() => setShowBudgetSetup(true)} 
-  className="flex justify-center items-center gap-[10px] rounded-[50px] bg-[#F3BE08] text-[15px] md:text-[20px] font-[400] leading-[140%] font-sans px-4 md:px-6 py-3 cursor-pointer border-none whitespace-nowrap"
+  className="flex justify-center items-center gap-[10px]  rounded-[15px]  bg-[#F3BE08] text-[15px] md:text-[20px] font-[400] leading-[140%] font-sans px-4 md:px-6 py-3 cursor-pointer border-none whitespace-nowrap"
 >
   {/* Text for desktop, icon for mobile */}
   <span className="hidden md:inline">Budget Setup</span>
   <svg 
-    className="md:hidden" 
+   
     xmlns="http://www.w3.org/2000/svg" 
     width="24" 
     height="24" 
@@ -1702,7 +1715,7 @@ function Budget() {
                 <div className="flex flex-col sm:flex-row gap-3 justify-center">
                   <button
                     onClick={() => navigate("/dashboard/budget/add-manual")}
-                    className="flex justify-center items-center min-w-[180px] h-[54px] gap-[10px] rounded-[15px] text-black font-sans text-[12px] md:text-[16px] font-[600] leading-[140%] cursor-pointer border-2 border-[#F39D08] px-6 transition-colors whitespace-nowrap"
+                    className="flex justify-center items-center min-w-[180px] h-[54px] gap-[10px] rounded-[15px] text-black font-sans text-[12px] md:text-[16px] font-[600] leading-[140%] cursor-pointer border-[1px] border-[rgba(0,0,0,0.20)] px-6 transition-colors whitespace-nowrap"
                   >
                     Add manually
                   </button>
@@ -1751,7 +1764,7 @@ function Budget() {
                   <div className="flex flex-row sm:flex-row gap-3 w-full lg:w-auto justify-center">
                     <button
                       onClick={() => navigate("/dashboard/budget/add-manual")}
-                      className="flex justify-center items-center min-w-[180px] h-[54px] gap-[10px] rounded-[15px] text-black font-sans text-[12px] md:text-[16px] font-[600] leading-[140%] cursor-pointer border-2 border-[#F39D08] px-6 transition-colors whitespace-nowrap"
+                      className="flex justify-center items-center min-w-[180px] h-[54px] gap-[10px] rounded-[15px] text-black font-sans text-[12px] md:text-[16px] font-[600] leading-[140%] cursor-pointer border-[1px] border-[rgba(0,0,0,0.20)] px-6 transition-colors whitespace-nowrap"
                     >
                       Add manually
                     </button>
